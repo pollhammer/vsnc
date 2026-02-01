8a118303ea1aa3c6500b7239bb9c5592e54791a4
Manuel Pollhammer <109719423+pollhammer@users.noreply.github.com>
2026-02-01 09:40:06 +0100

    Update to VSNC v1.3.bat
    update test

--- VSNC v1.3.bat
+++ VSNC v1.3.bat
@@ -4,7 +4,7 @@ reg add "HKCU\Console" /v VirtualTerminalLevel /t REG_DWORD /d 1 /f >nul 2>&1
 for /F %%a in ('echo prompt $E ^| cmd') do set "ESC=%%a"
 
 chcp 65001 >nul
-title VSNC v1.0
+title VSNC v1.3
 mode con: cols=59 lines=31
 
 setlocal EnableDelayedExpansion
@@ -20,7 +20,7 @@ echo %ESC%[38;5;196m                 ^|   /  ___/ / /^|  // /___
 echo %ESC%[38;5;124m                 ^|__/  /____/_/ ^|_/ \____/   
 echo %ESC%[0m
 
-echo                            v1.0
+echo                            v1.3
 echo                     Very Simple Net Chat     
 echo                 (2026) by Manuel Pollhammer
 echo.
@@ -55,4 +55,5 @@ goto loop
 
 :end
 exit
+
 

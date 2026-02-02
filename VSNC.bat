527fca5cb25f22a18fdd9ce5dc116de069aae3a6
Manuel Pollhammer <109719423+pollhammer@users.noreply.github.com>
2026-02-02 09:27:04 +0100

    vsnc

--- VSNC.bat
+++ VSNC.bat
@@ -2,11 +2,11 @@
 setlocal EnableDelayedExpansion
 
 :: Initialization
-reg add "HKCU\Console" /v VirtualTerminalLevel /t REG_DWORD /d 1 /f >nul 2>&1
 for /F %%a in ('echo prompt $E ^| cmd') do set "ESC=%%a"
+reg add "HKCU\Console" /v VirtualTerminalLevel /t REG_DWORD /d 1 /f >nul 2>&1
 
 chcp 65001 >nul
-title VSNC v1.2
+title VSNC v1.3
 mode con: cols=85 lines=30
 
 :: Chat directory for txt file
@@ -16,9 +16,6 @@ set "CHAT=C:\Temp"
 set "CHATFILE=%CHAT%\chat.txt"
 if not exist "%CHAT%" mkdir "%CHAT%"
 
-:: Date/time variable
-set "TS=%DATE%-%TIME:~0,5%"
-
 :: Header and branding
 echo.
 echo %ESC%[38;5;214m                              _   __ _____  _   __ ______
@@ -27,7 +24,7 @@ echo %ESC%[38;5;202m                             ^| ^|/ / \__ \ /  ^|/ // /
 echo %ESC%[38;5;196m                             ^|   /  ___/ / /^|  // /___   
 echo %ESC%[38;5;124m                             ^|__/  /____/_/ ^|_/ \____/   
 echo %ESC%[0m
-echo                                        v1.2
+echo                                        v1.3
 echo                               Very Simple Network Chat     
 echo                             (2026) by Manuel Pollhammer
 echo.
@@ -36,11 +33,14 @@ echo.
 set /p USER=Please enter your name: 
 if "%USER%"=="" set USER=Guest
 
+:: Random readable username color (no black)
+set /a USERCOLOR=17 + (%RANDOM% %% 215)
+
 :: Stabilize window after input
 mode con: cols=85 lines=30
 
 if not exist "%CHATFILE%" (
-    >>"%CHATFILE%" echo %ESC%[38;5;240m%ESC%[0m%ESC%[38;5;196m System:%ESC%[0m Chat started.
+    >>"%CHATFILE%" echo %ESC%[38;5;196m System:%ESC%[0m Chat started.
 )
 
 :: Main loop
@@ -73,18 +73,13 @@ goto loop
 
 :: Chat actions
 :write
-:: Ask user for text input
 set "TEXT="
-set /p TEXT=%ESC%[38;5;208m %USER%: %ESC%[0m
+set /p TEXT=%ESC%[38;5;%USERCOLOR%m %USER%:%ESC%[0m 
 
 if not "%TEXT%"=="" (
-    :: Capture date & time
-    set "TS=%DATE%-%TIME:~0,5%"
-    :: Write message to chat file
-    >>"%CHATFILE%" echo %ESC%[38;5;240m[%TS%]%ESC%[0m %ESC%[38;5;208m%USER%:%ESC%[0m %TEXT%
+    >>"%CHATFILE%" echo %ESC%[38;5;240m[%DATE%-%TIME:~0,5%]%ESC%[0m %ESC%[38;5;%USERCOLOR%m%USER%:%ESC%[0m %TEXT%
 )
 goto loop
 
-:: Exit program
 :end
 exit

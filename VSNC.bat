749cc60dde34e09aeedcb6076fe9b21072a4f35e
Manuel Pollhammer <109719423+pollhammer@users.noreply.github.com>
2026-02-02 06:21:04 +0100

    v1.2

--- VSNC.bat
+++ VSNC.bat
@@ -1,4 +1,7 @@
 @echo off
+setlocal EnableDelayedExpansion
+
+:: Initialization
 reg add "HKCU\Console" /v VirtualTerminalLevel /t REG_DWORD /d 1 /f >nul 2>&1
 for /F %%a in ('echo prompt $E ^| cmd') do set "ESC=%%a"
 
@@ -6,18 +9,17 @@ chcp 65001 >nul
 title VSNC v1.2
 mode con: cols=85 lines=30
 
-
-setlocal EnableDelayedExpansion
-
 :: Chat directory for txt file
-set CHAT=C:\Temp
+set "CHAT=C:\Temp"
 
 :: txt file name
-set CHATFILE=%CHAT%\chat.txt
+set "CHATFILE=%CHAT%\chat.txt"
+if not exist "%CHAT%" mkdir "%CHAT%"
 
 :: Date/time variable
 set "TS=%DATE%-%TIME:~0,5%"
 
+:: Header and branding
 echo.
 echo %ESC%[38;5;214m                              _   __ _____  _   __ ______
 echo %ESC%[38;5;208m                             ^| ^| / // ___/ / ^| / // ____/
@@ -25,12 +27,12 @@ echo %ESC%[38;5;202m                             ^| ^|/ / \__ \ /  ^|/ // /
 echo %ESC%[38;5;196m                             ^|   /  ___/ / /^|  // /___   
 echo %ESC%[38;5;124m                             ^|__/  /____/_/ ^|_/ \____/   
 echo %ESC%[0m
-
 echo                                        v1.2
 echo                                 Very Simple Net Chat     
 echo                             (2026) by Manuel Pollhammer
 echo.
 
+:: User login
 set /p USER=Please enter your name: 
 if "%USER%"=="" set USER=Guest
 
@@ -41,33 +43,38 @@ if not exist "%CHATFILE%" (
     >>"%CHATFILE%" echo %ESC%[38;5;240m%ESC%[0m%ESC%[38;5;196m System:%ESC%[0m Chat started.
 )
 
-
+:: Main loop
 :loop
 cls
 echo %ESC%[H
 echo ===== CHAT (latest 20) =====
+
 set /a LINECOUNT=0
-for /f "delims=" %%l in (%CHATFILE%) do (
+for /f "delims=" %%l in ('type "%CHATFILE%" 2^>nul') do (
     set /a LINECOUNT+=1
     set "LINE[!LINECOUNT!]=%%l"
 )
+
 set /a START=LINECOUNT-19
 if !START! lss 1 set START=1
-for /l %%i in (!START!,1,!LINECOUNT!) do echo !LINE[%%i]!
+
+for /l %%i in (!START!,1,!LINECOUNT!) do (
+    echo !LINE[%%i]!
+)
 
 echo.
 echo [W] write   [Q] quit
 
 choice /c WQR /n /t 1 /d R >nul
-
 if errorlevel 3 goto loop
 if errorlevel 2 goto end
 if errorlevel 1 goto write
-
 goto loop
 
+:: Chat actions
 :write
 :: Ask user for text input
+set "TEXT="
 set /p TEXT=%ESC%[38;5;208m %USER%: %ESC%[0m
 
 if not "%TEXT%"=="" (
@@ -78,8 +85,6 @@ if not "%TEXT%"=="" (
 )
 goto loop
 
-
+:: Exit program
 :end
 exit
-
-

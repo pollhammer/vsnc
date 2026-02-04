974545b96c10ac8fee5a0fc60704df0cab490625
Manuel Pollhammer <109719423+pollhammer@users.noreply.github.com>
2026-02-04 13:53:10 +0100

    vsnc

--- VSNC.bat
+++ VSNC.bat
@@ -1,6 +1,6 @@
+:: https://github.com/pollhammer/vsnc
 @echo off
 setlocal EnableDelayedExpansion
-:: https://github.com/pollhammer/vsnc
 
 :: Initialization
 for /F %%a in ('echo prompt $E ^| cmd') do set "ESC=%%a"
